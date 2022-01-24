9a0aa1888d5cf902d3884e9fb65214e6ebedf9fc
ZhongkaiSun <pipikai1204@gmail.com>
2022-01-24 10:42:52 -0500

    feat(common): add token release function

--- backend/common/jwt.go
+++ backend/common/jwt.go
@@ -35,3 +35,13 @@ func ReleaseToken(customer model.Customer) (string, error) {
 
 	return tokenString, nil
 }
+
+func ParseToken(tokenString string) (*jwt.Token, *Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (i interface{}, err error) {
+		return jwtKey, nil
+	})
+
+	return token, claims, err
+}
