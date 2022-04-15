982a22c1571aecf3534e8f4be468cea10667047e
RaindropZheng <zhengyudi@ufl.edu>
2022-04-15 15:54:33 -0400

    add cusine url

--- insert_data.sql
+++ insert_data.sql
@@ -63,75 +63,75 @@ insert into cuisines values ("Kavalanche", "Mai Kai Kava Llc", 8.50, 350, "pictu
 insert into cuisines values ("Loose Leaf Tea - 1oz", "Mai Kai Kava Llc", 5.00, 360, "picture/Mai_Kai_Kava_Llc/Loose_Leaf_Tea.jpeg");
 insert into cuisines values ("Micronized Kava", "Mai Kai Kava Llc", 20.00, 390, "picture/Mai_Kai_Kava_Llc/Micronized_Kava.jpeg");
 
-insert into cuisines values ("Tuna", "Subway", 8.90, 310);
-insert into cuisines values ("Spicy Italian", "Subway", 8.19, 310);
-insert into cuisines values ("Mozza Meat", "Subway", 9.37, 270);
-insert into cuisines values ("Subway Club", "Subway", 9.49, 150);
-insert into cuisines values ("B.L.T.", "Subway", 9.19, 210);
-insert into cuisines values ("Baja Steak & Jack", "Subway", 9.25, 350);
+insert into cuisines values ("Tuna", "Subway", 8.90, 310, "picture/Subway/Tuna.jpeg");
+insert into cuisines values ("Spicy Italian", "Subway", 8.19, 310, "picture/Subway/Spicy_Italian.jpeg");
+insert into cuisines values ("Mozza Meat", "Subway", 9.37, 270, "picture/Subway/Mozza_Meat.png");
+insert into cuisines values ("Subway Club", "Subway", 9.49, 150, "picture/Subway/Subway_Club.jpeg");
+insert into cuisines values ("B.L.T.", "Subway", 9.19, 210, "picture/Subway/BLT.jpeg");
+insert into cuisines values ("Baja Steak & Jack", "Subway", 9.25, 350, "picture/Subway/Baja_Steak_Jack.png");
 
-insert into cuisines values ("Chicken Caesar", "Just Salad", 11.29, 300);
-insert into cuisines values ("Crispy Chicken Poblano", "Just Salad", 11.99, 340);
-insert into cuisines values ("California Cobb", "Just Salad", 12.49, 310);
-insert into cuisines values ("Chicken Fajita Bowl", "Just Salad", 11.99, 320);
-insert into cuisines values ("Thai Chicken Crunch", "Just Salad", 12.29, 440);
-insert into cuisines values ("Buffalo Chicken", "Just Salad", 11.29, 450);
+insert into cuisines values ("Chicken Caesar", "Just Salad", 11.29, 300, "picture/Just_Salad/Chicken_Caesar.jpeg");
+insert into cuisines values ("Crispy Chicken Poblano", "Just Salad", 11.99, 340, "picture/Just_Salad/Crispy_Chicken_Poblano.jpeg");
+insert into cuisines values ("California Cobb", "Just Salad", 12.49, 310, "picture/Just_Salad/California_Cobb.jpeg");
+insert into cuisines values ("Chicken Fajita Bowl", "Just Salad", 11.99, 320, "picture/Just_Salad/Chicken_Fajita_Bowl.jpeg");
+insert into cuisines values ("Thai Chicken Crunch", "Just Salad", 12.29, 440, "picture/Just_Salad/Thai_Chicken_Crunch.jpeg");
+insert into cuisines values ("Buffalo Chicken", "Just Salad", 11.29, 450, "picture/Just_Salad/Buffalo_Chicken.jpeg");
 
-insert into cuisines values ("Burrito Bowl", "Chipotle", 9.45, 230);
-insert into cuisines values ("Chips & Guacamole", "Chipotle", 4.80, 330);
-insert into cuisines values ("Three Tacos", "Chipotle", 9.45, 340);
-insert into cuisines values ("Quesadilla", "Chipotle", 10.10, 230);
-insert into cuisines values ("Salad", "Chipotle", 9.45, 400);
-insert into cuisines values ("Burrito", "Chipotle", 9.45, 400);
+insert into cuisines values ("Burrito Bowl", "Chipotle", 9.45, 230, "picture/Chipotle/Burrito_Bowl.png");
+insert into cuisines values ("Chips & Guacamole", "Chipotle", 4.80, 330, "picture/Chipotle/Chips_Guacamole.png");
+insert into cuisines values ("Three Tacos", "Chipotle", 9.45, 340, "picture/Chipotle/Three_Tacos.jpeg");
+insert into cuisines values ("Quesadilla", "Chipotle", 10.10, 230, "picture/Chipotle/Quesadilla.png");
+insert into cuisines values ("Salad", "Chipotle", 9.45, 400, "picture/Chipotle/Salad.jpeg");
+insert into cuisines values ("Burrito", "Chipotle", 9.45, 400, "picture/Chipotle/Burrito.png");
 
-insert into cuisines values ("Crunchwrap Supreme", "TacoBell", 6.23, 230);
-insert into cuisines values ("Chicken Quesadilla", "TacoBell", 5.99, 400);
-insert into cuisines values ("Beefy 5-Layer Burrito", "TacoBell", 4.19, 350);
-insert into cuisines values ("Cheesy Gordita Crunch", "TacoBell", 5.27, 400);
-insert into cuisines values ("Nachos BellGrande", "TacoBell", 6.59, 420);
-insert into cuisines values ("Steak Quesadilla", "TacoBell", 6.23, 450);
+insert into cuisines values ("Crunchwrap Supreme", "TacoBell", 6.23, 230, "picture/TacoBell/Crunchwrap_Supreme.jpeg");
+insert into cuisines values ("Chicken Quesadilla", "TacoBell", 5.99, 400, "picture/TacoBell/Chicken_Quesadilla.jpeg");
+insert into cuisines values ("Beefy 5-Layer Burrito", "TacoBell", 4.19, 350, "picture/TacoBell/Beef_5Layer_Burrito.jpeg");
+insert into cuisines values ("Cheesy Gordita Crunch", "TacoBell", 5.27, 400, "picture/TacoBell/Cheesy_Gordita_Crunch.jpeg");
+insert into cuisines values ("Nachos BellGrande", "TacoBell", 6.59, 420, "picture/TacoBell/Nachos_BellGrande.jpeg");
+insert into cuisines values ("Steak Quesadilla", "TacoBell", 6.23, 450, "picture/TacoBell/Steak_Quesadilla.jpeg");
 
-insert into cuisines values ("Build Your Own Pizza (11-inch)", "Blaze Pizza", 10.95, 400);
-insert into cuisines values ("Cheesy Bread", "Blaze Pizza", 5.95, 400);
-insert into cuisines values ("Pesto Garlic Cheesy Bread", "Blaze Pizza", 5.95, 340);
-insert into cuisines values ("1 Top Pizza (11-inch)", "Blaze Pizza", 8.65, 600);
-insert into cuisines values ("Simple Salad - Side", "Blaze Pizza", 6.85, 500);
-insert into cuisines values ("Simple Pie (11-inch)", "Blaze Pizza", 7.65, 340);
+insert into cuisines values ("Build Your Own Pizza (11-inch)", "Blaze Pizza", 10.95, 400, "picture/Blaze_Pizza/Build_Your_Own_Pizza.jpeg");
+insert into cuisines values ("Cheesy Bread", "Blaze Pizza", 5.95, 400, "picture/Blaze_Pizza/Cheesy_Bread.jpeg");
+insert into cuisines values ("Pesto Garlic Cheesy Bread", "Blaze Pizza", 5.95, 340, "picture/Blaze_Pizza/Pesto_Garlic_Cheesy_Bread.jpeg");
+insert into cuisines values ("1 Top Pizza (11-inch)", "Blaze Pizza", 8.65, 600, "picture/Blaze_Pizza/1_Top_Pizza.jpeg");
+insert into cuisines values ("Simple Salad - Side", "Blaze Pizza", 6.85, 500, "picture/Blaze_Pizza/Simple_Salad_Side.jpeg");
+insert into cuisines values ("Simple Pie (11-inch)", "Blaze Pizza", 7.65, 340, "picture/Blaze_Pizza/Simple_Pie.jpeg");
 
-insert into cuisines values ("Meat Lover's", "Pizza Hut", 19.19, 500);
-insert into cuisines values ("Supreme Pizza", "Pizza Hut", 19.19, 300);
-insert into cuisines values ("Pepperoni Lover's", "Pizza Hut", 19.19, 430);
-insert into cuisines values ("Veggie Lover's", "Pizza Hut", 19.19, 500);
-insert into cuisines values ("Buffalo Chicken", "Pizza Hut", 19.19, 400);
-insert into cuisines values ("Backyard BBQ Chicken", "Pizza Hut", 19.19, 600);
+insert into cuisines values ("Meat Lover's", "Pizza Hut", 19.19, 500, "picture/Pizza_Hut/Meat_Lover.jpeg");
+insert into cuisines values ("Supreme Pizza", "Pizza Hut", 19.19, 300, "picture/Pizza_Hut/Supreme_Pizza.jpeg");
+insert into cuisines values ("Pepperoni Lover's", "Pizza Hut", 19.19, 430, "picture/Pizza_Hut/Pepperoni_Lover.jpeg");
+insert into cuisines values ("Veggie Lover's", "Pizza Hut", 19.19, 500, "picture/Pizza_Hut/Veggie_Lover.jpeg");
+insert into cuisines values ("Buffalo Chicken", "Pizza Hut", 19.19, 400, "picture/Pizza_Hut/Buffalo_Chicken.jpeg");
+insert into cuisines values ("Backyard BBQ Chicken", "Pizza Hut", 19.19, 600 ,"picture/Pizza_Hut/Backyard_BBQ_Chicken.jpeg");
 
-insert into cuisines values ("4 pc. Chicken Combo", "KFC", 12.59, 340);
-insert into cuisines values ("Famous Bowl Fill Up", "KFC", 7.55, 500);
-insert into cuisines values ("Famous Bowl", "KFC", 6.83, 450);
-insert into cuisines values ("3 pc. Chicken Box", "KFC", 11.99, 500);
-insert into cuisines values ("5 pc. Tenders Box", "KFC", 13.55, 450);
-insert into cuisines values ("8 Piece Chicken", "KFC", 20.99, 560);
+insert into cuisines values ("4 pc. Chicken Combo", "KFC", 12.59, 340, "picture/KFC/Chicken_Combo.jpeg");
+insert into cuisines values ("Famous Bowl Fill Up", "KFC", 7.55, 500, "picture/KFC/Famous_Bowl_Fill_Up.jpeg");
+insert into cuisines values ("Famous Bowl", "KFC", 6.83, 450, "picture/KFC/Famous_Bowl.png");
+insert into cuisines values ("3 pc. Chicken Box", "KFC", 11.99, 500, "picture/KFC/Chicken_Box.jpeg");
+insert into cuisines values ("5 pc. Tenders Box", "KFC", 13.55, 450, "picture/KFC/Tenders_Box.jpeg");
+insert into cuisines values ("8 Piece Chicken", "KFC", 20.99, 560, "picture/KFC/Piece_Chicken.jpeg");
 
-insert into cuisines values ("Crispy Chicken Sandwich", "McDonald's", 4.70, 470);
-insert into cuisines values ("Big Mac Meal", "McDonald's", 9.23, 440);
-insert into cuisines values ("Double Quarter Pounder with Cheese Meal", "McDonald's", 10.55, 450);
-insert into cuisines values ("20 Piece McNuggets", "McDonald's", 12.19, 830);
-insert into cuisines values ("10 Piece McNuggets Meal", "McDonald's", 10.43, 500);
-insert into cuisines values ("French Fries", "McDonald's", 2.49, 220);
+insert into cuisines values ("Crispy Chicken Sandwich", "McDonald's", 4.70, 470, "picture/McDonald's/Crispy_Chicken_Sandwich.jpeg");
+insert into cuisines values ("Big Mac Meal", "McDonald's", 9.23, 440, "picture/McDonald's/Big_Mac_Meal.jpeg");
+insert into cuisines values ("Double Quarter Pounder with Cheese Meal", "McDonald's", 10.55, 450, "picture/McDonald's/Double_Quarter_Pounder.jpeg");
+insert into cuisines values ("20 Piece McNuggets", "McDonald's", 12.19, 830, "picture/McDonald's/McNuggets.jpeg");
+insert into cuisines values ("10 Piece McNuggets Meal", "McDonald's", 10.43, 500, "picture/McDonald's/McNuggets_Meal.jpeg");
+insert into cuisines values ("French Fries", "McDonald's", 2.49, 220,"picture/McDonald's/French_Fries.jpeg");
 
-insert into cuisines values ("Strawberry & Cream Cheese Crispers", "Bakery By Perkins", 5.29, 450);
-insert into cuisines values ("Chocolate French Silk Pie", "Bakery By Perkins", 17.29, 460);
-insert into cuisines values ("Chocolate Overload Cake", "Bakery By Perkins", 4.59, 470);
-insert into cuisines values ("Raspberry Lemon Drop", "Bakery By Perkins", 5.79, 480);
-insert into cuisines values ("Lemon Meringue Pie", "Bakery By Perkins", 4.59, 490);
-insert into cuisines values ("Coconut Cream Pie", "Bakery By Perkins", 4.59, 500);
+insert into cuisines values ("Strawberry & Cream Cheese Crispers", "Bakery By Perkins", 5.29, 450, "picture/Bakery_By_Perkins/StrawberryCream_Cheese_Crispers.jpeg");
+insert into cuisines values ("Chocolate French Silk Pie", "Bakery By Perkins", 17.29, 460, "picture/Bakery_By_Perkins/Chocolate_French_Silk_Pie.jpeg");
+insert into cuisines values ("Chocolate Overload Cake", "Bakery By Perkins", 4.59, 470 , "picture/Bakery_By_Perkins/Chocolate_Overload_Cake.jpeg");
+insert into cuisines values ("Raspberry Lemon Drop", "Bakery By Perkins", 5.79, 480, "picture/Bakery_By_Perkins/Raspberry_Lemon_Drop.jpeg");
+insert into cuisines values ("Lemon Meringue Pie", "Bakery By Perkins", 4.59, 490, "picture/Bakery_By_Perkins/Lemon_Meringue_Pie.jpeg");
+insert into cuisines values ("Coconut Cream Pie", "Bakery By Perkins", 4.59, 500, "picture/Bakery_By_Perkins/Coconut_Cream_Pie.jpeg");
 
-insert into cuisines values ("1 Mixer + 2 Toppings", "Rock n roll", 7.99, 400);
-insert into cuisines values ("Crepe with up to 5 toppings", "Rock n roll", 8.99, 420);
-insert into cuisines values ("Mango Sorbet", "Rock n roll", 5.25, 430);
-insert into cuisines values ("2 Mixer + 3 Toppings", "Rock n roll", 8.99, 440);
-insert into cuisines values ("Waffle with up to 5 toppings", "Rock n roll", 8.99, 450);
-insert into cuisines values ("Hot Chocolate", "Rock n roll", 4.50, 600);
+insert into cuisines values ("1 Mixer + 2 Toppings", "Rock n roll", 7.99, 400, "picture/Rock_n_roll/1Mixer2Toppings.jpeg");
+insert into cuisines values ("Crepe with up to 5 toppings", "Rock n roll", 8.99, 420, "picture/Rock_n_roll/Crepe_with_5toppings.jpeg");
+insert into cuisines values ("Mango Sorbet", "Rock n roll", 5.25, 430, "picture/Rock_n_roll/Mango_Sorbet.jpeg");
+insert into cuisines values ("2 Mixer + 3 Toppings", "Rock n roll", 8.99, 440, "picture/Rock_n_roll/2Mixer3Toppings.jpeg");
+insert into cuisines values ("Waffle with up to 5 toppings", "Rock n roll", 8.99, 450,"picture/Rock_n_roll/Waffle_with_5toppings .jpeg");
+insert into cuisines values ("Hot Chocolate", "Rock n roll", 4.50, 600, "picture/Rock_n_roll/Hot_Chocolate.jpeg");
 
 
 
