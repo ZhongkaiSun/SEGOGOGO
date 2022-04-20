a0fa98f4529fa7ae3ff14c86874231aa9ef24e53
HenryLiu96 <liuhongru96@gmail.com>
2022-04-20 14:26:23 -0400

    add test cases

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -9,7 +9,25 @@ describe('Home Page Test', function () {
         cy.get('form')
         cy.title().should('eq', 'GatorDash')
     })
+    it('test nav bar', function () {
+        cy.get('nav')
+            .find('a[id="History"]')
+            .click()
+        cy.url().should('include', 'History')
+        cy.go('back')
 
+        cy.get('input[type = "Search"]')
+            .invoke('attr', 'placeholder')
+            .should('contain', 'Search')
+
+        cy.get('input[type = "Search"]').type("Popeyes")
+
+        cy.get('a[id = "search"]')
+            .should('have.text','Search')
+            .click()
+        cy.url().should('include','Restaurant')
+        cy.go('back')
+    });
     it('test the login and logout', function () {
         cy.get('nav')
             .find('a[id="Login"]')
@@ -23,5 +41,39 @@ describe('Home Page Test', function () {
             .find('a[id="Logout"]')
             .click()
     });
+    it('test Cuisine category', function () {
+        cy.get('#american')  // get the containing toolbar
+            .find('img[src*="american.png"]')     // *= gives a partial match on src
+            .click()
+        cy.get('#category').contains("American")
+        cy.go('back')
 
+        cy.get('#coffeeandtea')  // get the containing toolbar
+            .find('img[src*="Coffee.png"]')     // *= gives a partial match on src
+            .click()
+        cy.get('#category').contains("Coffee")
+        cy.go('back')
+
+    });
+
+    it('test cart', function () {
+        cy.get('#cd-cart-trigger').click()
+        cy.get('#cd-cart')
+            .find('a[id = "checkout"]')
+            .click()
+        cy.on('window:alert', (txt) => {
+            //Mocha assertions
+            expect(txt).to.contains('Please add something into your cart! Or login first.');
+        })
+        cy.visit('./Front-end/newHome.html')
+    });
+
+    it('test favorite restaurants', function () {
+        cy.get('#fav1')
+            .contains('Popeyes')
+        cy.get('#fav1')
+            .contains('Order Now')
+
+
+    });
 })
